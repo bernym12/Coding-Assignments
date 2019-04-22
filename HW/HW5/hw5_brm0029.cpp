1fd28afb788a2532ea4ff704ea33a5eb48ad268b
bernym12 <bernym12@users.noreply.github.com>
2019-04-22 10:04:12 -0500

    Added addTriviaNode functionalityty

--- HW/HW5/hw5_brm0029.cpp
+++ HW/HW5/hw5_brm0029.cpp
@@ -13,51 +13,73 @@ struct TriviaNode
 
 };
 
-TriviaNode addQuestion(string question, string answer, int points, TriviaNode* next, TriviaNode* head);
+void addQuestion(string question, string answer, int points, TriviaNode* &head, TriviaNode* &tail);
+void initialTrivia(TriviaNode* &head, TriviaNode* &tail);
 int main() {
-	TriviaNode* trivia;
-	TriviaNode current;
-	trivia = &current;
-	string player = "Bernie";
-	string response;
-	string question;
-	string answer;
-	int points;	
-	cout<<"* * * Welcome to "<<player<<"'s trivia quiz game * * *"<<endl;
-	cout<<"Enter a question: ";
-	getline(cin,question);
-	cout<<"\nEnter an answer: ";
-	getline(cin,answer);
-	cout<<"\nEnter award points: ";
-	cin>>points;
-	//current = addQuestion(question,response,points,current,trivia);
-	// trivia = &current;
-	cout<<"Continue? (Yes/No): ";
-	getline(cin,response);
-	// current.next = trivia;
-	TriviaNode* prev = &current;
-	 while(response.compare("no") != 0) {
-	 	// trivia = current
-		// TriviaNode current;
-		cout<<"Enter a question: ";
-		getline(cin,question);
+	TriviaNode* trivia = NULL;
+	TriviaNode* tail = NULL;
+	// string player = "Bernie";
+	// string response;
+	// string question;
+	// string answer;
+	// int points;	
+	// cout<<"* * * Welcome to "<<player<<"'s trivia quiz game * * *"<<endl;
+	// cout<<"Enter a question: ";
+	// getline(cin,question);
 
-		cout<<"\nEnter an answer: ";
-		getline(cin,answer);
-		cout<<"\nEnter award points: ";
-		cin>>points;
-		//current = addQuestion(question,response,points, current.next);
-		prev->next = &current;
-		prev = &current;
-		cout<<"Continue? (Yes/No): ";
-		getline(cin,response);
-	 }
+	// cout<<"\nEnter an answer: ";
+	// getline(cin,answer);
 
+	// cout<<"\nEnter award points: ";
+	// cin>>points;
+
+	// cout<<"Continue? (Yes/No): ";
+	// getline(cin,response);
+
+	//  while(response.compare("no") != 0) {
+	// 	cout<<"Enter a question: ";
+	// 	getline(cin,question);
+
+	// 	cout<<"\nEnter an answer: ";
+	// 	getline(cin,answer);
+	// 	cout<<"\nEnter award points: ";
+	// 	cin>>points;
+	// 	cout<<"Continue? (Yes/No): ";
+	// 	getline(cin,response);
+	//  }
+	initialTrivia(trivia, tail);
+	cout<<trivia->question<<endl;
+	//TriviaNode* access;
+	//access = trivia->next;
+	//cout<<tail<<endl;
+	//cout<<access->question<<endl;
+	//access = access->next;
+	//while( (access->next) != NULL) {
+		
+	//}
 }
 
-TriviaNode addQuestion(string question, string answer, int points, TriviaNode* next, TriviaNode* Head) {
-	TriviaNode q1 = {question, answer, points,next};
-	return q1;
+void addQuestion(string question, string answer, int points, TriviaNode* &head, TriviaNode* &tail) {
+	TriviaNode* q1 = new TriviaNode;
+	q1->question = question;
+	q1->answer = answer;
+	q1->points = points;
+
+	
+	if (head == NULL) {	
+		head = q1;
+		tail = q1;		
+	}
+	else {
+		tail->next = q1;
+		tail = q1;
+	}
+}
+void initialTrivia(TriviaNode* &head, TriviaNode* &tail) {
+	
+	addQuestion("How long was the shortest war on record? (Hint: how many minutes)", "38", 100, head, tail);
+	addQuestion("What was Bank of America's original name? (Hint: Bank of Italy or Bank of Germany)", "Bank of Italy", 50, head, tail);
+	addQuestion("What is the best-selling video game of all time? (Hint: Minecraft or Tetris)", "Tetris", 20, head, tail);
 }
 
 
