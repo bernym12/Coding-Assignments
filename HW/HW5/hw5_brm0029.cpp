1c0e34a6faf094efe263d65d619e6d1144394bef
bernym12 <bernym12@users.noreply.github.com>
2019-04-22 19:44:28 -0500

    Added cin.ignore() before getline calls to remove bug. Also added debug mode with #define, #ifdef, #else

--- HW/HW5/hw5_brm0029.cpp
+++ HW/HW5/hw5_brm0029.cpp
@@ -18,41 +18,14 @@ struct TriviaNode
 int addQuestion(string question, string answer, int points, TriviaNode* &head, TriviaNode* &tail);
 void initialTrivia(TriviaNode* &head, TriviaNode* &tail);
 int askQuestion(TriviaNode* &head, int numToAsk);
-void test_addQuestion(void);
-
+void test_askQuestion(void);
+// #define UNIT_TESTING
 #ifdef	UNIT_TESTING
-
-#define UNIT_TESTING
-
-void test_addQuestion(void) {
-	TriviaNode* head = NULL;
-	TriviaNode* tail = NULL;
-	initialTrivia(head, tail);
-	cout<<"*** This is a debug version ***"<<endl;
-	cout<<"Unit Test Case 1: Ask no questions. The program should give a warning message."<<endl;
-	askQuestion(head, 0);
-	cout<<"Unit Test Case 2.1: Ask 1 question in the linked list. The tester enters an incorrect answer."<<endl;
-	assert(0 == askQuestion(head,1));
-	cout<<"Case 2.1 passed...";
-	cout<<"Unit Test Case 2.2: Ask 1 question in the linked list. The tester enters a correct answer."<<endl;
-	assert(0 == askQuestion(head,1));
-	cout<<"Case 2.2 passed...";
-	cout<<"Unit Test Case 3.1: Ask 2 questions in the linked list. The tester enters an incorrect answer."<<endl;
-	assert(0 == askQuestion(head,2));
-	cout<<"Case 3.1 passed...";
-	cout<<"Unit Test Case 3.2: Ask 2 questions in the linked list. The tester enters a correct answer."<<endl;
-	assert(0 == askQuestion(head,2));
-	cout<<"Case 3.2 passed...";
-	cout<<"Unit Test Case 4.1: Ask 3 questions in the linked list. The tester enters an incorrect answer."<<endl;
-	assert(0 == askQuestion(head,3));
-	cout<<"Case 4.1 passed...";
-	cout<<"Unit Test Case 4.2: Ask 3 questions in the linked list. The tester enters a correct answer."<<endl;
-	assert(0 == askQuestion(head,3));
-	cout<<"Case 4.2 passed...";
-	cout<<"Unit Test Case 5: Ask five questions in the linked list."<<endl;
-	askQuestion(head,5);
-	cout<<"\n*** End of the Debug Version ***"<<endl;
+int main() {
+	test_askQuestion();
 }
+
+
 #else
 int main() {
 	TriviaNode* trivia = NULL;
@@ -66,32 +39,24 @@ int main() {
 	string question;
 	string answer;
 	int points;	
-	
-	// cout<<"Enter a question: ";
-	// getline(cin,question);
-
-	// cout<<"\nEnter an answer: ";
-	// getline(cin,answer);
-
-	// cout<<"\nEnter award points: ";
-	// cin>>points;
-	// addQuestion(question, answer, points, ptrivia, ptail);
-	// cout<<"Continue? (Yes/No): ";
-	// getline(cin,response);
 
 	do {
 		cout<<"Enter a question: ";
+		cin.ignore();
 		getline(cin,question);
-		cout<<"\nEnter an answer: ";
+		cout<<"Enter an answer: ";
+		cin.ignore();
 		getline(cin,answer);
-		cout<<"\nEnter award points: ";
+		cout<<"Enter award points: ";
 		cin>>points;
 		addQuestion(question, answer, points, ptrivia, ptail);
 		cout<<"Continue? (Yes/No): ";
-		cin>>response;	
-	 } while(response.compare("no") != 0);
-	 
+		cin.ignore();
+		getline(cin,response);	
+	 } while(response.compare("Yes") == 0);
+	 cout<<"\n";	
 	 askQuestion(trivia, 3);
+	 cout<<"\n*** Thank you for playing the trivia quiz game. Goodbye! ***";
 }
 #endif
 int addQuestion(string question, string answer, int points, TriviaNode* &head, TriviaNode* &tail) {
@@ -119,23 +84,23 @@ int addQuestion(string question, string answer, int points, TriviaNode* &head, T
 int askQuestion(TriviaNode* &head, int numToAsk) {
 	
 	if (numToAsk < 1) {
-		cout<<"Warning - The number of trivia to be asked must be equal to or larger than 1\n"<<endl;
+		cout<<"Warning - The number of trivia to be asked must be equal to or larger than 1.\n"<<endl;
 		return 1;
 	}
 	if (numToAsk > 3) {
 		cout<<"Warning - The are only three trivia in the list.\n"<<endl;
 		return 1;
 	}
+
 	TriviaNode* access = new TriviaNode;
 	string answer;
 	access = head;
 	
 	int playerPoints = 0;
-	
 	for(int start = 0; access != NULL && start < numToAsk; access = access->next, start++) {
 		cout<<"Question: "<<access->question<<endl;
 		cout<<"Answer: ";
-		getline(cin,answer);
+		getline(cin, answer);
 
 		if ((access->answer).compare(answer) == 0) {
 			cout<<"Your answer is correct. You receive: "<<access->points<<" points"<<endl;
@@ -146,7 +111,7 @@ int askQuestion(TriviaNode* &head, int numToAsk) {
 			cout<<"Your answer is wrong. The correct answer is: "<<access->answer<<endl;
 		}
 
-		cout<<"Your total points is: "<<playerPoints<<endl;
+		cout<<"Your total points is: "<<playerPoints<<"\n"<<endl;
 	}
 	return 0;
 }
@@ -157,6 +122,35 @@ void initialTrivia(TriviaNode* &head, TriviaNode* &tail) {
 	addQuestion("What is the best-selling video game of all time? (Hint: Minecraft or Tetris)", "Tetris", 20, head, tail);
 }
 
+void test_askQuestion(void) {
+	TriviaNode* head = NULL;
+	TriviaNode* tail = NULL;
+	initialTrivia(head, tail);
+	cout<<"*** This is a debug version ***"<<endl;
+	cout<<"Unit Test Case 1: Ask no questions. The program should give a warning message."<<endl;
+	askQuestion(head, 0);
+	cout<<"Unit Test Case 2.1: Ask 1 question in the linked list. The tester enters an incorrect answer."<<endl;
+	assert(0 == askQuestion(head,1));
+	cout<<"Case 2.1 passed..."<<endl;
+	cout<<"Unit Test Case 2.2: Ask 1 question in the linked list. The tester enters a correct answer."<<endl;
+	assert(0 == askQuestion(head,1));
+	cout<<"Case 2.2 passed..."<<endl;
+	cout<<"Unit Test Case 3.1: Ask 2 questions in the linked list. The tester enters an incorrect answer."<<endl;
+	assert(0 == askQuestion(head,2));
+	cout<<"Case 3.1 passed..."<<endl;
+	cout<<"Unit Test Case 3.2: Ask 2 questions in the linked list. The tester enters a correct answer."<<endl;
+	assert(0 == askQuestion(head,2));
+	cout<<"Case 3.2 passed..."<<endl;
+	cout<<"Unit Test Case 4.1: Ask 3 questions in the linked list. The tester enters an incorrect answer."<<endl;
+	assert(0 == askQuestion(head,3));
+	cout<<"Case 4.1 passed..."<<endl;
+	cout<<"Unit Test Case 4.2: Ask 3 questions in the linked list. The tester enters a correct answer."<<endl;
+	assert(0 == askQuestion(head,3));
+	cout<<"Case 4.2 passed..."<<endl;
+	cout<<"Unit Test Case 5: Ask five questions in the linked list."<<endl;
+	askQuestion(head,5);
+	cout<<"\n*** End of the Debug Version ***"<<endl;
+}
 
 
 
